8f8359a3aaeae52957d938dcd93ba2e78f484e4e
Rahulsurya1 <121539105+Rahulsurya1@users.noreply.github.com>
2023-06-23 15:14:53 +0530

    Update variables.tf

--- terraform_code/variables.tf
+++ terraform_code/variables.tf
@@ -2,11 +2,4 @@ variable "resource_group_location" {
   type        = string
   default     = "eastus"
   description = "Location of the resource group."
-}
-
-variable "resource_group_name" {
-  type        = string
-  default     = "etpx-rg"
-  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
-}
-              
+}           
