7064c25a381df3b9a5f61d3132b03114860a7c1d
Rahulsurya1 <121539105+Rahulsurya1@users.noreply.github.com>
2023-06-23 15:16:11 +0530

    Update main.tf

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = apim-rg-${var.environment}
+  name     = "apim-rg-${var.environment}"
 }
 
 # Create virtual network
