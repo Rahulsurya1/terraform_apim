337398bb4ff78c3b22e88738ce4462ba3af36b18
Rahulsurya1 <121539105+Rahulsurya1@users.noreply.github.com>
2023-06-27 15:29:18 +0530

    Update providers.tf

--- apim-tf/providers.tf
+++ apim-tf/providers.tf
@@ -9,16 +9,8 @@ terraform {
 
   required_providers {
     azurerm = {
+      version = "~>3.2"
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-    tls = {
-      source = "hashicorp/tls"
-      version = "~>4.0"
     }
   }
 }
