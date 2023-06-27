7d9a9dfdcfce19478d355d64af0e18255860b4d1
Rahulsurya1 <121539105+Rahulsurya1@users.noreply.github.com>
2023-06-27 11:14:14 +0530

    Create providers.tf

---
+++ apim-tf/providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.3"
+  backend "azurerm" {
+    resource_group_name  = "tfstate-files"
+    storage_account_name = "tfstatefiles9"
+    container_name       = "tfstate2"
+    key                  = "actions.tfstate2"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    tls = {
+      source = "hashicorp/tls"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
