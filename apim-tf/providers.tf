5fea955158721b51108f68ebf39508d128e3598b
Rahulsurya1 <121539105+Rahulsurya1@users.noreply.github.com>
2023-06-28 13:48:44 +0530

    Update providers.tf

--- apim-tf/providers.tf
+++ apim-tf/providers.tf
@@ -3,8 +3,8 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-files"
     storage_account_name = "tfstatefiles9"
-    container_name       = "tfstate2"
-    key                  = "actions.tfstate2"
+    container_name       = "tfstate"
+    key                  = "actions.tfstate"
   }
 
   required_providers {
