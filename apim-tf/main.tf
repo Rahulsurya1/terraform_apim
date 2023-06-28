2c69643fe33d4051f5dd4c49434c4cdc3817bca5
Rahulsurya1 <121539105+Rahulsurya1@users.noreply.github.com>
2023-06-28 13:49:52 +0530

    Update main.tf

--- apim-tf/main.tf
+++ apim-tf/main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_resource_group" "etpx-rg" {
-  name     = "eptx-apim-workflow"
+  name     = "eptx-apim-workflow1"
   location = "eastus2"
 }
 
 #API Management creation
 
 resource "azurerm_api_management" "apimservice" {
-  name                = "ETPX-service"
+  name                = "ETPX-service-1"
   location            = azurerm_resource_group.etpx-rg.location
   resource_group_name = azurerm_resource_group.etpx-rg.name
   publisher_name      = "etpxse"
@@ -18,7 +18,7 @@ resource "azurerm_api_management_custom_domain" "custdom" {
   api_management_id = azurerm_api_management.apimservice.id
 
   gateway {
-    host_name            = "ETPX-APIM"
+    host_name            = "ETPX-APIM-1"
     key_vault_id         = null
     certificate          = null
     certificate_password = null
@@ -29,7 +29,7 @@ resource "azurerm_api_management_custom_domain" "custdom" {
 }
 #create application insights
 resource "azurerm_application_insights" "insight" {
-  name                = "eptx-app-insight"
+  name                = "eptx-app-insight-1"
   location            = azurerm_resource_group.etpx-rg.location
   resource_group_name = azurerm_resource_group.etpx-rg.name
   workspace_id        = azurerm_log_analytics_workspace.log.id
@@ -38,7 +38,7 @@ resource "azurerm_application_insights" "insight" {
 
 #create log analytics workspace
 resource "azurerm_log_analytics_workspace" "log" {
-  name                = "etpx-log-analytics"
+  name                = "etpx-log-analytics-1"
   location            = azurerm_resource_group.etpx-rg.location
   resource_group_name = azurerm_resource_group.etpx-rg.name
   sku                 = "PerGB2018"
@@ -48,7 +48,7 @@ resource "azurerm_log_analytics_workspace" "log" {
 }
 
 resource "azurerm_api_management_logger" "logger" {
-  name                = "etpx-apimlogger"
+  name                = "etpx-apimlogger-1"
   api_management_name = azurerm_api_management.apimservice.name
   resource_group_name = azurerm_resource_group.etpx-rg.name
   
