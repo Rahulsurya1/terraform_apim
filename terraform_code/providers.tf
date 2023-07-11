29fa1675438cb5821b75e5fd7d4906233f56f1f8
Rahulsurya1 <121539105+Rahulsurya1@users.noreply.github.com>
2023-07-11 17:00:59 +0530

    Update providers.tf

--- terraform_code/providers.tf
+++ terraform_code/providers.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "local" {
-    path = terraform_apim/terraform_apim/terraform_code
+    path = /home/ubuntu/actions-runner/_work/terraform_apim/terraform_apim/terraform_code
   }
   required_providers {
     azurerm = {
